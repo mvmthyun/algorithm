f87726ed5de44bd405201009dcb0007852f5d12e
mvmthyun <mvmthyun@gmail.com>
2023-06-12 17:32:40 +0900

    #1 백준 알고리즘 1일차 11022번 문제 풀이

---
+++ 11022.py
@@ -0,0 +1,15 @@
+
+# T = int(input())
+# for i in range(1, T+1):
+#     a, b= map(int,input().split())
+#     print(f'Case #{i}: {a+b}')
+
+
+a = 1
+b = 1
+T = 5
+
+# T = int(input())
+for i in range(1, T+1):
+    # a, b= map(int,input().split())
+    print(f'Case #{i}: {a} + {b} = {a+b}')
